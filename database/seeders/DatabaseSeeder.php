7da344151d1a539505a25906b9b681493ff81b1f
fajardihati <fajardihati@gmail.com>
2025-10-27 22:32:02 +0700

    Initial commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,9 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'name' => 'Code with Bahri',
-            'email' => 'bahri@fic11.com',
-            'password' => Hash::make('12345678'),
+            'name' => 'PATITECH',
+            'email' => 'admin@patitech.id',
+            'password' => Hash::make('patitech12345'),
         ]);
 
 
